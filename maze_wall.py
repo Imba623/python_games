11d24d79c4e62ef86b7ad895a34bc23d3c654fa0
Alex <72447021+Imba623@users.noreply.github.com>
2026-01-31 12:22:07 +1000

    Create maze_wall.py

---
+++ maze_wall.py
@@ -0,0 +1,28 @@
+wall1 = Rect(100, 100, 200, 20)
+wall1_direction = 1 # 1 = вправо, -1 = влево
+
+
+# Внутренние стены
+wall2 = Rect(150, 100, 20, 150)   # Вертикальная 1
+wall2_direction = 1
+
+wall3 =Rect(400, 50, 200, 20)   # Горизонтальная 1
+wall3_direction = 1
+
+wall4 = Rect(350, 100, 20, 200)   # Вертикальная 2
+wall4_direction = 1
+
+wall5 = Rect(200, 250, 150, 20)   # Горизонтальная 2
+wall5_direction = 1
+
+wall6 = Rect(200, 250, 20, 150)   # Вертикальная 3
+wall6_direction = 1
+
+wall7 = Rect(300, 400, 200, 20)  # Горизонтальная 3
+wall7_direction = 1
+
+wall8 = Rect(400, 400, 20, 200)  # Вертикальная 4
+wall8_direction = 1
+
+wall9 = Rect(450, 150, 200, 20)  # Горизонтальная 4
+wall9_direction = 1
